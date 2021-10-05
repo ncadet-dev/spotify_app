8f9e21c5e51952cf11fa592380f990c95b01ea53
Nico <c.nicolas334@gmail.com>
2021-10-05 10:34:05 +0200

    Modified the settings to differentiate debug from production environments

--- manage.py
+++ manage.py
@@ -6,7 +6,10 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'spotify_app.settings')
+    os.environ.setdefault(
+        'DJANGO_SETTINGS_MODULE',
+        'spotify_app.settings.debug'
+    )
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
